95d2fadc95efdb990636aaaee8bc56921bed1468
FlaxiGG <mickza050@gmail.com>
2024-12-17 20:41:21 +0700

    Change PDF to PNG

--- app/exLoader/page.tsx
+++ app/exLoader/page.tsx
@@ -13,13 +13,13 @@ export default function EXLoader() {
     <>
       <form className="max-w-sm mx-auto">
         <label
-          htmlFor="countries"
+          htmlFor="select-term"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
           ตารางการเรียน - การสอน IT411 - 441 DEK65
         </label>
         <select
-          id="countries"
+          id="select-term"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={selectedFile}
           onChange={handleSelectChange}
@@ -42,18 +42,21 @@ export default function EXLoader() {
           <option value="a" disabled>
             ปีที่ 3 เทอม 1 [On Work]
           </option>
-          <option value="TreeTermTwo.pdf">ปีที่ 3 เทอม 2 (วิชาเลือก หัวข้อพิเศษ)</option>
-          <option value="TreeTermTwo2.pdf">ปีที่ 3 เทอม 2 (วิชาเลือก ถ่ายรูป) [On Work]</option>
+          <option value="TreeTermTwo.png">
+            ปีที่ 3 เทอม 2 (วิชาเลือก หัวข้อพิเศษ)
+          </option>
+          <option value="TreeTermTwo2.png">
+            ปีที่ 3 เทอม 2 (วิชาเลือก ถ่ายรูป)
+          </option>
         </select>
       </form>
 
       {selectedFile && (
-        <div className="mt-4">
+        <div className="mx-auto w-full mt-4">
           <iframe
             src={`/pdf/${selectedFile}`}
             width="100%"
             height="600px"
-            className="border rounded"
             title="PDF Viewer"
           ></iframe>
         </div>
